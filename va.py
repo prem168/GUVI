d7e9294102781b59b516b1df30f387ebcbd96cff
Prem sai vardhan varudu <36048842+prem168@users.noreply.github.com>
2019-03-19 15:48:37 +0530

    Create va.py

---
+++ va.py
@@ -0,0 +1,10 @@
+# PSV
+ch=input()
+m=['a','e','i','o','u','A','E','I','O','U']
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+	if(ch in m):
+		print('Vowel')
+	else:
+		print('Constant')
+else:
+	print('invalid')

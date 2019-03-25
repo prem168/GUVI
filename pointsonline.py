4254c9538dc4c6058fb093081ddb16c5650066fe
Prem sai vardhan varudu <36048842+prem168@users.noreply.github.com>
2019-03-25 11:22:30 +0530

    Create pointsonline.py

---
+++ pointsonline.py
@@ -0,0 +1,7 @@
+x=list(map(int,input().split()))
+y=list(map(int,input().split()))
+z=list(map(int,input().split()))
+if((x[0]==y[0]==z[0]) or (x[1]==y[1]==z[1])):
+    print("yes")
+else:
+    print("no")

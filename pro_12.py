43c5a97319ba87e85a3915aa6c095d20e4dfe90f
Prem sai vardhan varudu <36048842+prem168@users.noreply.github.com>
2019-03-30 21:25:16 +0530

    Update pro_12.py

--- pro_12.py
+++ pro_12.py
@@ -1,15 +1,12 @@
-x,y=input().split(" ")
+x,y=input().split()
 x=int(x)
 y=int(y)
-m=list(map(int,input().split(" ")))
 n=[]
+m=list(map(int,input().split()))
 for i in range(0,y):
-    sum=0
     u,v=input().split(" ")
     u=int(u)
     v=int(v)
-    for j in range(u-1,v):
-        sum+=m[j]
-    n.append(sum)
+    n.append(sum(m[u-1:v]))
 for i in range(0,len(n)):
     print(n[i])

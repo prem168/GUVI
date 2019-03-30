5b5455ff1715e402276b2bea7cc465b2920ea656
Prem sai vardhan varudu <36048842+prem168@users.noreply.github.com>
2019-03-30 21:26:50 +0530

    Create pro_13.py

---
+++ pro_13.py
@@ -0,0 +1,12 @@
+x,y=input().split()
+x=int(x)
+y=int(y)
+n=[]
+m=list(map(int,input().split()))
+for i in range(0,y):
+    u,v=input().split(" ")
+    u=int(u)
+    v=int(v)
+    n.append(min(m[u-1:v]))
+for i in range(0,len(n)):
+    print(n[i])

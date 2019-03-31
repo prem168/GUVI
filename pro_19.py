0359a3190bb8e300a0c41d297b82ecaea259739f
Prem sai vardhan varudu <36048842+prem168@users.noreply.github.com>
2019-03-31 12:29:34 +0530

    Update pro_19.py

--- pro_19.py
+++ pro_19.py
@@ -5,7 +5,9 @@ for i in range(0,x):
     m=list(map(int,input().split(" ")))
     n=n+m
 n.sort()
-for i in range(0,len(n)-1):
-    print(n[i],end=" ")
+l=len(n)
+if(l>1):
+    for i in range(0,l-1):
+        print(n[i],end=" ")
 
-print(n[i+1])
+print(n[l-1])

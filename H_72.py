74709426beb243df9096f6dfb713ede10b267745
Prem sai vardhan varudu <36048842+prem168@users.noreply.github.com>
2019-05-11 23:50:17 +0530

    Update H_72.py

--- H_72.py
+++ H_72.py
@@ -2,6 +2,9 @@ x=list(input().split(" "))
 l=len(x)
 for i in range(0,l,2):
     x[i]=''.join(reversed(x[i]))
-for i in range(0,l-2):
-    print(x[i],end=" ")
+if(l!=2):
+    for i in range(0,l-2):
+        print(x[i],end=" ")
+else:
+    print(x[0],end=" ")
 print(x[l-1])
